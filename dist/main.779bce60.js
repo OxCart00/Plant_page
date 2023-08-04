bfc01c5c70f5b8f1b029e08c787557ac2dc668f1
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-08-04 04:11:29 -0600

    working

--- dist/main.779bce60.js
+++ dist/main.779bce60.js
@@ -153,30 +153,53 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // Obtener el botón "Get" por su ID
 var getButton = document.querySelector(".get--button");
 
+// Obtener todos los elementos de formulario por su clase
+var formElements = document.getElementsByClassName("plants--form__answere");
+
+// Obtener el formulario y los campos requeridos
+var formulario = document.querySelector('.plants--form');
+
 // Agregar un evento de clic al botón "Get"
 getButton.addEventListener("click", function (event) {
   event.preventDefault(); // Evitar el envío del formulario
 
-  // Obtener todos los elementos de formulario por su clase
-  var formElements = document.getElementsByClassName("plants--form__answere");
-
   // Crear un array para almacenar los IDs de los elementos seleccionados
   var selectedIds = [];
 
-  // Recorrer los elementos de formulario para verificar cuáles están seleccionados
-  for (var i = 0; i < formElements.length; i++) {
-    var element = formElements[i];
-    if (element.checked) {
-      selectedIds.push(element.id);
+  // Si todos los campos están llenos, enviar el formulario
+  if (formulario.checkValidity()) {
+    // Recorrer los elementos de formulario para verificar cuáles están seleccionados
+    for (var i = 0; i < formElements.length; i++) {
+      var element = formElements[i];
+      if (element.checked) {
+        selectedIds.push(element.id);
+      }
     }
-  }
-  if (selectedIds.length != 0) {
-    var newPlant = new _plantBuilder2.default({ location: selectedIds[0], soil: selectedIds[1], toxicity: selectedIds[2], pot: selectedIds[3], style: selectedIds[4] });
-    // Aquí puedes realizar cualquier acción que desees con los IDs de los elementos seleccionados
-    console.log("IDs de elementos seleccionados:", newPlant);
+    if (selectedIds.length >= 5) {
+      var newPlant = new _plantBuilder2.default({ location: selectedIds[0], soil: selectedIds[1], toxicity: selectedIds[2], pot: selectedIds[3], style: selectedIds[4] });
+      if (selectedIds.length > 5) {
+        for (var _i = 5; _i < selectedIds.length; _i++) {
+          if (selectedIds[_i] === "moss-pole") {
+            newPlant.addMoss();
+          }
+          if (selectedIds[_i] === "pebbles") {
+            newPlant.addPebbles();
+          }
+          if (selectedIds[_i] === "mini-plants") {
+            newPlant.addGreenies();
+          }
+        }
+      }
+      // Aquí puedes realizar cualquier acción que desees con los IDs de los elementos seleccionados
+      console.log("Plant Recomendation:", newPlant);
+      console.log("IDs de elementos seleccionados:", selectedIds);
+    }
+  } else {
+    // Si no están completados, mostrar un mensaje de error
+    alert('Por favor, completa todos los campos obligatorios.');
   }
 });
-},{"./Modules/plant-builder":10}],12:[function(require,module,exports) {
+},{"./Modules/plant-builder":10}],15:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -346,5 +369,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[12,6], null)
+},{}]},{},[15,6], null)
 //# sourceMappingURL=/main.779bce60.map
