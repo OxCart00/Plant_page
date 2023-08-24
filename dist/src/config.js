ea3a9d0970bfef09c11a3d70312530005961e310
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-08-24 15:10:07 -0600

    format

--- dist/src/config.js
+++ dist/src/config.js
@@ -5,22 +5,22 @@ export const stockConfig = {
     ceramic: {
       simple: {
         painted: `ceramic-simple-${plantInfo.color}`,
-        unpainted: "ceramic-simple-unpainted"
+        unpainted: "ceramic-simple-unpainted",
       },
       decorated: {
         painted: `ceramic-decorated-${plantInfo.color}`,
-        unpainted: "ceramic-decorated-unpainted"
-      }
+        unpainted: "ceramic-decorated-unpainted",
+      },
     },
     clay: {
       simple: {
         painted: `clay-simple-painted-${plantInfo.color}`,
-        unpainted: "clay-simple-unpainted"
+        unpainted: "clay-simple-unpainted",
       },
       decorated: {
         painted: `clay-decorated-${plantInfo.color}`,
-        unpainted: "clay-decorated-unpainted"
-      }
-    }
-  }
+        unpainted: "clay-decorated-unpainted",
+      },
+    },
+  },
 };
--- dist/src/modules/accordion-build.js
+++ dist/src/modules/accordion-build.js
@@ -50,25 +50,31 @@ function createAndAppendElement(tag, text, container) {
   element.textContent = text;
   container.appendChild(element);
 }
-
 function handleInfo(data) {
   console.log("Datos de la respuesta:", data);
   createAndAppendElement("p", data.description, infoAccordion);
   createAndAppendElement("p", `HUMIDITY: ${data.care.humidity}`, tipsAccordion);
   createAndAppendElement("p", `LIGHT: ${data.care.light}`, tipsAccordion);
-  createAndAppendElement("p", `TEMPERATURE: ${data.care.temperature}`, tipsAccordion);
+  createAndAppendElement(
+    "p",
+    `TEMPERATURE: ${data.care.temperature}`,
+    tipsAccordion,
+  );
   createAndAppendElement("p", `WATER: ${data.care.water}`, tipsAccordion);
 }
 export async function getObjectInformation() {
   const plantInfo = JSON.parse(localStorage.getItem("plant-info"));
-
   const clayPot = plantInfo.pot.split("-").includes("clay");
   const potType = clayPot ? "clay" : "ceramic";
   const isDecorated = plantInfo.pot.split("-").includes("decorated");
   const isPainted = plantInfo.pot.split("-").includes("painted");
-  const plantName = plantInfo.plant === "peace-lily" ? "peaceLily" : plantInfo.plant;  
-  const potId = stockConfig.potOptions[potType][isDecorated ? "decorated" : "simple"][isPainted ? "painted" : "unpainted"];
-  
+  const plantName =
+    plantInfo.plant === "peace-lily" ? "peaceLily" : plantInfo.plant;
+  const potId =
+    stockConfig.potOptions[potType][isDecorated ? "decorated" : "simple"][
+      isPainted ? "painted" : "unpainted"
+    ];
+
   const objectPromises = [
     fetchPlantInfo("plant", plantName),
     fetchPlantInfo("pot", potId),
@@ -82,14 +88,15 @@ export async function getObjectInformation() {
     );
     for (let i = 0; i < objectsInfo.length; i++) {
       const objectType = i === 0 ? "plant" : i === 1 ? "pot" : "soil";
-      const objectName = i === 0 ? plantInfo.plant : i === 1 ? potId : plantInfo.soil;
-      
+      const objectName =
+        i === 0 ? plantInfo.plant : i === 1 ? potId : plantInfo.soil;
+
       if (objectsInfo[i].stock <= 10 && objectsInfo[i].stock !== 0) {
         const alertMessage = `${objectName} ${objectType}: Only ${objectsInfo[i].stock} items left on stock`;
         const alert = createAlert(alertMessage, "yellow");
         inventAccordion.appendChild(alert);
       }
-      
+
       if (objectsInfo[i].stock === 0) {
         const alertMessage = `${objectName} ${objectType} is out of stock, please select a different ${objectType}`;
         const alert = createAlert(alertMessage, "red");
@@ -103,17 +110,17 @@ export async function getObjectInformation() {
         ? "https://qfble0gquj.execute-api.us-east-2.amazonaws.com/plant-store/info/peaceLily"
         : `https://qfble0gquj.execute-api.us-east-2.amazonaws.com/plant-store/info/${plantInfo.plant}`;
 
-        fetch(descriptionUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then(handleInfo)
-        .catch((error) => {
-          console.error("Error al obtener los datos:", error);
-        });
+    fetch(descriptionUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(handleInfo)
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
   } catch (error) {
     console.error("Error al obtener información de objetos:", error);
   }
